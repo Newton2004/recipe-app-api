de71ea23c48218ef6a9ddaf7c748b1a357d84088
Newton raja K <knewtonraja@gmail.com>
2025-01-18 18:31:12 +0530

    ssss

--- app/core/models.py
+++ app/core/models.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
+
 class UserManager(BaseUserManager):
     """Manager for users."""
 
@@ -20,7 +21,6 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
-
 
     def create_superuser(self, email, password):
         """Create and return a new superuser."""
